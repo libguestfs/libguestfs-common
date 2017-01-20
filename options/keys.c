8d0062cd70d132495303c3b9cde8ff10a375d08d
Richard W.M. Jones <rjones@redhat.com>
2017-01-20 11:24:49 +0000

    fish: Move fishcommon library to common/options.
    This is mostly code motion but:
    
    (1) I had to remove the compile-time COMPILING_GUESTFISH and
    COMPILING_VIRT_RESCUE macros and replace them with runtime constants
    and checks.
    
    (2) I moved the fish/config.c file into this library.

---
+++ options/keys.c
@@ -0,0 +1,96 @@
+/* libguestfs - guestfish and guestmount shared option parsing
+ * Copyright (C) 2010-2012 Red Hat Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <termios.h>
+#include <string.h>
+#include <libintl.h>
+
+#include "guestfs.h"
+
+#include "options.h"
+
+/**
+ * Read a passphrase ('Key') from F</dev/tty> with echo off.
+ *
+ * The caller (F<fish/cmds.c>) will call free on the string
+ * afterwards.  Based on the code in cryptsetup file F<lib/utils.c>.
+ */
+char *
+read_key (const char *param)
+{
+  FILE *infp, *outfp;
+  struct termios orig, temp;
+  char *ret = NULL;
+  int tty;
+  int tcset = 0;
+  size_t allocsize = 0;
+  ssize_t len;
+
+  /* Read and write to /dev/tty if available. */
+  if (keys_from_stdin ||
+      (infp = outfp = fopen ("/dev/tty", "w+")) == NULL) {
+    infp = stdin;
+    outfp = stdout;
+  }
+
+  /* Print the prompt and set no echo. */
+  tty = isatty (fileno (infp));
+  if (tty) {
+    fprintf (outfp, _("Enter key or passphrase (\"%s\"): "), param);
+
+    if (!echo_keys) {
+      if (tcgetattr (fileno (infp), &orig) == -1) {
+        perror ("tcgetattr");
+        goto error;
+      }
+      memcpy (&temp, &orig, sizeof temp);
+      temp.c_lflag &= ~ECHO;
+
+      tcsetattr (fileno (infp), TCSAFLUSH, &temp);
+      tcset = 1;
+    }
+  }
+
+  len = getline (&ret, &allocsize, infp);
+  if (len == -1) {
+    perror ("getline");
+    ret = NULL;
+    goto error;
+  }
+
+  /* Remove the terminating \n if there is one. */
+  if (len > 0 && ret[len-1] == '\n')
+    ret[len-1] = '\0';
+
+ error:
+  /* Restore echo, close file descriptor. */
+  if (tty && tcset) {
+    printf ("\n");
+    tcsetattr (fileno (infp), TCSAFLUSH, &orig);
+  }
+
+  if (infp != stdin)
+    fclose (infp); /* outfp == infp, so this is closed also */
+
+  return ret;
+}
---
+++ options/options.h
@@ -0,0 +1,232 @@
+/* libguestfs - guestfish and guestmount shared option parsing
+ * Copyright (C) 2010-2012 Red Hat Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef OPTIONS_H
+#define OPTIONS_H
+
+#include <config.h>
+
+#include <stdbool.h>
+
+#include "guestfs-internal-frontend.h"
+
+/* Provided by guestfish or guestmount. */
+extern guestfs_h *g;
+extern int read_only;
+extern int live;
+extern int verbose;
+extern int inspector;
+extern int keys_from_stdin;
+extern int echo_keys;
+extern const char *libvirt_uri;
+extern int in_guestfish;
+extern int in_virt_rescue;
+
+/* List of drives added via -a, -d or -N options.  NB: Unused fields
+ * in this struct MUST be zeroed, ie. use calloc, not malloc.
+ */
+struct drv {
+  struct drv *next;
+
+  char *device;    /* Device name inside the appliance (eg. /dev/sda).
+                    * This is filled in when we add the drives in
+                    * add_drives.  Note that guests (-d option) may
+                    * have multiple drives, in which case this is the
+                    * first drive, and nr_drives is the number of
+                    * drives used.
+                    */
+  int nr_drives;   /* number of drives for this guest */
+
+  enum {
+    drv_a,                      /* -a option (without URI) */
+    drv_uri,                    /* -a option (with URI) */
+    drv_d,                      /* -d option */
+    drv_N,                      /* -N option (guestfish only) */
+    drv_scratch,                /* --scratch option (virt-rescue only) */
+  } type;
+  union {
+    struct {
+      char *filename;       /* disk filename */
+      const char *format;   /* format (NULL == autodetect) */
+      const char *cachemode;/* cachemode (NULL == default) */
+      const char *discard;  /* discard (NULL == disable) */
+    } a;
+    struct {
+      char *path;           /* disk path */
+      char *protocol;       /* protocol (eg. "nbd") */
+      char **server;        /* server(s) - can be NULL */
+      char *username;       /* username - can be NULL */
+      char *password;       /* password - can be NULL */
+      const char *format;   /* format (NULL == autodetect) */
+      const char *orig_uri; /* original URI (for error messages etc.) */
+    } uri;
+    struct {
+      char *guest;          /* guest name */
+    } d;
+    struct {
+      char *filename;       /* disk filename (testX.img) */
+      void *data;           /* prepared type */
+      void (*data_free)(void*); /* function to free 'data' */
+    } N;
+    struct {
+      int64_t size;         /* size of the disk in bytes */
+    } scratch;
+  };
+
+  /* Opaque pointer.  Not used by the options-parsing code, and so
+   * available for the program to use for any purpose.
+   */
+  void *opaque;
+};
+
+struct mp {
+  struct mp *next;
+  char *device;
+  char *mountpoint;
+  char *options;
+  char *fstype;
+};
+
+/* in config.c */
+extern void parse_config (void);
+
+/* in decrypt.c */
+extern void inspect_do_decrypt (guestfs_h *g);
+
+/* in domain.c */
+extern int add_libvirt_drives (guestfs_h *g, const char *guest);
+
+/* in inspect.c */
+extern void inspect_mount_handle (guestfs_h *g);
+extern void inspect_mount_root (guestfs_h *g, const char *root);
+#define inspect_mount() inspect_mount_handle (g)
+extern void print_inspect_prompt (void);
+
+/* in key.c */
+extern char *read_key (const char *param);
+
+/* in options.c */
+extern void option_a (const char *arg, const char *format, struct drv **drvsp);
+extern void option_d (const char *arg, struct drv **drvsp);
+extern char add_drives_handle (guestfs_h *g, struct drv *drv, char next_drive);
+#define add_drives(drv, next_drive) add_drives_handle (g, drv, next_drive)
+extern void mount_mps (struct mp *mp);
+extern void free_drives (struct drv *drv);
+extern void free_mps (struct mp *mp);
+
+#define OPTION_a                                \
+  do {                                          \
+  option_a (optarg, format, &drvs);             \
+  format_consumed = true;                       \
+  } while (0)
+
+#define OPTION_A                                \
+  do {                                          \
+    option_a (optarg, format, &drvs2);          \
+    format_consumed = true;                     \
+  } while (0)
+
+#define OPTION_c                                \
+  libvirt_uri = optarg
+
+#define OPTION_d                                \
+  option_d (optarg, &drvs)
+
+#define OPTION_D                                \
+  option_d (optarg, &drvs2)
+
+#define OPTION_format                           \
+  do {                                          \
+    if (!optarg || STREQ (optarg, ""))          \
+      format = NULL;                            \
+    else                                        \
+      format = optarg;                          \
+    format_consumed = false;                    \
+  } while (0)
+
+#define OPTION_i                                \
+  inspector = 1
+
+#define OPTION_m                                \
+  mp = malloc (sizeof (struct mp));             \
+  if (!mp)                                      \
+    error (EXIT_FAILURE, errno, "malloc");      \
+  mp->fstype = NULL;                            \
+  mp->options = NULL;                           \
+  mp->mountpoint = (char *) "/";                \
+  p = strchr (optarg, ':');                     \
+  if (p) {                                      \
+    *p = '\0';                                  \
+    p++;                                        \
+    mp->mountpoint = p;                         \
+    p = strchr (p, ':');                        \
+    if (p) {                                    \
+      *p = '\0';                                \
+      p++;                                      \
+      mp->options = p;                          \
+      p = strchr (p, ':');                      \
+      if (p) {                                  \
+        *p = '\0';                              \
+        p++;                                    \
+        mp->fstype = p;                         \
+      }                                         \
+    }                                           \
+  }                                             \
+  mp->device = optarg;                          \
+  mp->next = mps;                               \
+  mps = mp
+
+#define OPTION_n                                \
+  guestfs_set_autosync (g, 0)
+
+#define OPTION_r                                \
+  read_only = 1
+
+#define OPTION_v                                \
+  verbose++;                                    \
+  guestfs_set_verbose (g, verbose)
+
+#define OPTION_V                                                        \
+  {                                                                     \
+    printf ("%s %s\n",                                                  \
+            getprogname (),                                             \
+            PACKAGE_VERSION_FULL);                                      \
+    exit (EXIT_SUCCESS);                                                \
+  }
+
+#define OPTION_w                                                        \
+  if (read_only) {                                                      \
+    fprintf (stderr, _("%s: cannot mix --ro and --rw options\n"),       \
+             getprogname ());                                           \
+    exit (EXIT_FAILURE);                                                \
+  }
+
+#define OPTION_x                                \
+  guestfs_set_trace (g, 1)
+
+#define CHECK_OPTION_format_consumed                                    \
+  do {                                                                  \
+    if (!format_consumed) {                                             \
+      fprintf (stderr,                                                  \
+               _("%s: --format parameter must appear before -a parameter\n"), \
+               getprogname ());                                         \
+      exit (EXIT_FAILURE);                                              \
+    }                                                                   \
+  } while (0)
+
+#endif /* OPTIONS_H */
