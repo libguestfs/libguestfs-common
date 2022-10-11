libguestfs/libguestfs-common

commit f84d95474ccf873a09eb0df9b54554a2d84a886b

Introduce a --key option in tools that accept keys
The majority of the tools have already options (--echo-keys &
--keys-from-stdin) to deal with LUKS credentials, although there is no
way to automatically provide credentials.  --keys-from-stdin is
suboptimal, because it is a usable solution only when there is just one
device to open, and no other input passed via stdin to the tool (like
the commands for guestfish).

To overcome this limitation, introduce a new --key option in tools:
* --key /dev/device:file:/filename/with/key
* --key /dev/device:string:the-actual-key
this way it is possible to pass all the credentials needed for the
specific devices to open, with no risk of conflict with stdin, and also
in a secure way (when using the "file" way).

On the technical side: this adds a new "key_store" API for the C tools,
making sure it is used only when needed.  Partially mirror it also for
the OCaml tools, although there will be a conversion to the C API
because the decryption helpers used are in the common C parts.

options/keys.c
@@ -24,6 +24,9 @@
 #include <termios.h>
 #include <string.h>
 #include <libintl.h>
+#include <errno.h>
+#include <error.h>
+#include <assert.h>
 
 #include "guestfs.h"
 
@@ -93,4 +96,156 @@ read_key (const char *param)
     fclose (infp); /* outfp == infp, so this is closed also */
 
   return ret;
+}
+
+static char *
+read_first_line_from_file (const char *filename)
+{
+  CLEANUP_FCLOSE FILE *fp = NULL;
+  char *ret = NULL;
+  size_t allocsize = 0;
+  ssize_t len;
+
+  fp = fopen (filename, "r");
+  if (!fp)
+    error (EXIT_FAILURE, errno, "fopen: %s", filename);
+
+  len = getline (&ret, &allocsize, fp);
+  if (len == -1)
+    error (EXIT_FAILURE, errno, "getline: %s", filename);
+
+  /* Remove the terminating \n if there is one. */
+  if (len > 0 && ret[len-1] == '\n')
+    ret[len-1] = '\0';
+
+  return ret;
+}
+
+char *
+get_key (struct key_store *ks, const char *device)
+{
+  size_t i;
+
+  if (ks) {
+    for (i = 0; i < ks->nr_keys; ++i) {
+      struct key_store_key *key = &ks->keys[i];
+      char *s;
+
+      if (STRNEQ (key->device, device))
+        continue;
+
+      switch (key->type) {
+      case key_string:
+        s = strdup (key->string.s);
+        if (!s)
+          error (EXIT_FAILURE, errno, "strdup");
+        return s;
+      case key_file:
+        return read_first_line_from_file (key->file.name);
+      }
+
+      /* Key not found in the key store, ask the user for it. */
+      break;
+    }
+  }
+
+  return read_key (device);
+}
+
+struct key_store *
+key_store_add_from_selector (struct key_store *ks, const char *selector_orig)
+{
+  CLEANUP_FREE char *selector = strdup (selector_orig);
+  const char *elem;
+  char *saveptr;
+  struct key_store_key key;
+
+  if (!selector)
+    error (EXIT_FAILURE, errno, "strdup");
+
+  /* 1: device */
+  elem = strtok_r (selector, ":", &saveptr);
+  if (!elem) {
+   invalid_selector:
+    error (EXIT_FAILURE, 0, "invalid selector for --key: %s", selector_orig);
+  }
+  key.device = strdup (elem);
+  if (!key.device)
+    error (EXIT_FAILURE, errno, "strdup");
+
+  /* 2: key type */
+  elem = strtok_r (NULL, ":", &saveptr);
+  if (!elem)
+    goto invalid_selector;
+  else if (STREQ (elem, "key"))
+    key.type = key_string;
+  else if (STREQ (elem, "file"))
+    key.type = key_file;
+  else
+    goto invalid_selector;
+
+  /* 3: actual key */
+  elem = strtok_r (NULL, ":", &saveptr);
+  if (!elem)
+    goto invalid_selector;
+  switch (key.type) {
+  case key_string:
+    key.string.s = strdup (elem);
+    if (!key.string.s)
+      error (EXIT_FAILURE, errno, "strdup");
+    break;
+  case key_file:
+    key.file.name = strdup (elem);
+    if (!key.file.name)
+      error (EXIT_FAILURE, errno, "strdup");
+    break;
+  }
+
+  return key_store_import_key (ks, &key);
+}
+
+struct key_store *
+key_store_import_key (struct key_store *ks, const struct key_store_key *key)
+{
+  struct key_store_key *new_keys;
+
+  if (!ks) {
+    ks = calloc (1, sizeof (*ks));
+    if (!ks)
+      error (EXIT_FAILURE, errno, "strdup");
+  }
+  assert (ks != NULL);
+
+  new_keys = realloc (ks->keys, sizeof (*ks->keys) + 1);
+  if (!new_keys)
+    error (EXIT_FAILURE, errno, "realloc");
+
+  ks->keys = new_keys;
+  ks->keys[ks->nr_keys] = *key;
+  ++ks->nr_keys;
+
+  return ks;
+}
+
+void
+free_key_store (struct key_store *ks)
+{
+  size_t i;
+
+  if (!ks)
+    return;
+
+  for (i = 0; i < ks->nr_keys; ++i) {
+    struct key_store_key *key = &ks->keys[i];
+
+    switch (key->type) {
+    case key_string:
+      free (key->string.s);
+      break;
+    case key_file:
+      free (key->file.name);
+      break;
+    }
+    free (key->device);
+  }
 }

mltools/tools_utils-c.c
@@ -22,6 +22,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
+#include <error.h>
 
 #include <caml/alloc.h>
 #include <caml/fail.h>
@@ -33,7 +34,7 @@
 
 #include "options.h"
 
-extern value guestfs_int_mllib_inspect_decrypt (value gv, value gpv);
+extern value guestfs_int_mllib_inspect_decrypt (value gv, value gpv, value keysv);
 extern value guestfs_int_mllib_set_echo_keys (value unitv);
 extern value guestfs_int_mllib_set_keys_from_stdin (value unitv);
 
@@ -42,12 +43,47 @@ int echo_keys = 0;
 int keys_from_stdin = 0;
 
 value
-guestfs_int_mllib_inspect_decrypt (value gv, value gpv)
+guestfs_int_mllib_inspect_decrypt (value gv, value gpv, value keysv)
 {
-  CAMLparam2 (gv, gpv);
+  CAMLparam3 (gv, gpv, keysv);
+  CAMLlocal2 (elemv, v);
   guestfs_h *g = (guestfs_h *) (intptr_t) Int64_val (gpv);
+  struct key_store *ks = NULL;
 
-  inspect_do_decrypt (g);
+  while (keysv != Val_emptylist) {
+    struct key_store_key key;
+
+    elemv = Field (keysv, 0);
+    key.device = strdup (String_val (Field (elemv, 0)));
+    if (!key.device)
+      caml_raise_out_of_memory ();
+
+    v = Field (elemv, 1);
+    switch (Tag_val (v)) {
+    case 0:  /* KeyString of string */
+      key.type = key_string;
+      key.string.s = strdup (String_val (Field (v, 0)));
+      if (!key.string.s)
+        caml_raise_out_of_memory ();
+      break;
+    case 1:  /* KeyFileName of string */
+      key.type = key_file;
+      key.file.name = strdup (String_val (Field (v, 0)));
+      if (!key.file.name)
+        caml_raise_out_of_memory ();
+      break;
+    default:
+      error (EXIT_FAILURE, 0,
+             "internal error: unhandled Tag_val (v) = %d",
+             Tag_val (v));
+    }
+
+    ks = key_store_import_key (ks, &key);
+
+    keysv = Field (keysv, 1);
+  }
+
+  inspect_do_decrypt (g, ks);
 
   CAMLreturn (Val_unit);
 }

mltools/tools_utils.ml
@@ -22,7 +22,14 @@ open Std_utils
 open Common_gettext.Gettext
 open Getopt.OptionName
 
-external c_inspect_decrypt : Guestfs.t -> int64 -> unit = "guestfs_int_mllib_inspect_decrypt"
+type key_store = {
+  keys : (string, key_store_key) Hashtbl.t;
+}
+and key_store_key =
+  | KeyString of string
+  | KeyFileName of string
+
+external c_inspect_decrypt : Guestfs.t -> int64 -> (string * key_store_key) list -> unit = "guestfs_int_mllib_inspect_decrypt"
 external c_set_echo_keys : unit -> unit = "guestfs_int_mllib_set_echo_keys" "noalloc"
 external c_set_keys_from_stdin : unit -> unit = "guestfs_int_mllib_set_keys_from_stdin" "noalloc"
 
@@ -261,6 +268,7 @@ let machine_readable () =
 
 type cmdline_options = {
   getopt : Getopt.t;
+  ks : key_store;
 }
 
 let create_standard_options argspec ?anon_fun ?(key_opts = false) ?(machine_readable = false) usage_msg =
@@ -288,6 +296,9 @@ let create_standard_options argspec ?anon_fun ?(key_opts = false) ?(machine_read
         error (f_"invalid output for --machine-readable: %s") fmt
       )
   in
+  let ks = {
+    keys = Hashtbl.create 13;
+  } in
   let argspec = [
     [ S 'V'; L"version" ], Getopt.Unit print_version_and_exit, s_"Display version and exit";
     [ S 'v'; L"verbose" ], Getopt.Unit set_verbose,  s_"Enable libguestfs debugging messages";
@@ -300,9 +311,20 @@ let create_standard_options argspec ?anon_fun ?(key_opts = false) ?(machine_read
   let argspec =
     argspec @
       (if key_opts then
+      let parse_key_selector arg =
+        let parts = String.nsplit ~max:3 ":" arg in
+        match parts with
+        | [ device; "key"; key ] ->
+          Hashtbl.replace ks.keys device (KeyString key)
+        | [ device; "file"; file ] ->
+          Hashtbl.replace ks.keys device (KeyFileName file)
+        | _ ->
+          error (f_"invalid selector string for --key: %s") arg
+      in
       [
         [ L"echo-keys" ],       Getopt.Unit c_set_echo_keys,       s_"Don’t turn off echo for passphrases";
         [ L"keys-from-stdin" ], Getopt.Unit c_set_keys_from_stdin, s_"Read passphrases from stdin";
+        [ L"key" ], Getopt.String (s_"SELECTOR", parse_key_selector), s_"Specify a LUKS key";
       ]
       else []) @
       (if machine_readable then
@@ -312,7 +334,7 @@ let create_standard_options argspec ?anon_fun ?(key_opts = false) ?(machine_read
       else []) in
   let getopt = Getopt.create argspec ?anon_fun usage_msg in
   {
-    getopt;
+    getopt; ks;
   }
 
 (* Run an external command, slurp up the output as a list of lines. *)
@@ -599,13 +621,21 @@ let is_btrfs_subvolume g fs =
     if g#last_errno () = Guestfs.Errno.errno_EINVAL then false
     else raise exn
 
-let inspect_decrypt g =
+let inspect_decrypt g ks =
+  (* Turn the keys in the key_store into a simpler struct, so it is possible
+   * to read it using the C API.
+   *)
+  let keys_as_list = Hashtbl.fold (
+    fun k v acc ->
+      (k, v) :: acc
+  ) ks.keys [] in
   (* Note we pass original 'g' even though it is not used by the
    * callee.  This is so that 'g' is kept as a root on the stack, and
    * so cannot be garbage collected while we are in the c_inspect_decrypt
    * function.
    *)
   c_inspect_decrypt g#ocaml_handle (Guestfs.c_pointer g#ocaml_handle)
+    keys_as_list
 
 let with_timeout op timeout ?(sleep = 2) fn =
   let start_t = Unix.gettimeofday () in

mltools/tools_utils.mli
@@ -74,8 +74,11 @@ val machine_readable : unit -> machine_readable_fn option
     readable output to, in case it was enabled via
     [--machine-readable]. *)
 
+type key_store
+
 type cmdline_options = {
   getopt : Getopt.t;              (** The actual {!Getopt} handle. *)
+  ks : key_store;                 (** Container for keys read via [--key]. *)
 }
 (** Structure representing all the data needed for handling command
     line options. *)
@@ -85,7 +88,10 @@ val create_standard_options : Getopt.speclist -> ?anon_fun:Getopt.anon_fun -> ?k
     sorting them, and setting [long_options] to them.
 
     [key_opts] specifies whether add the standard options related to
-    keys management, i.e. [--echo-keys] and [--keys-from-stdin].
+    keys management, i.e. [--echo-keys], [--key], and [--keys-from-stdin].
+    In case [key_opts] is specified, {!recfield:cmdline_options.ks} will
+    contain the keys specified via [--key], so it ought to be passed around
+    where needed.
 
     [machine_readable] specifies whether add the [--machine-readable]
     option.
@@ -188,7 +194,7 @@ val inspect_mount_root_ro : Guestfs.guestfs -> string -> unit
 val is_btrfs_subvolume : Guestfs.guestfs -> string -> bool
 (** Checks if a filesystem is a btrfs subvolume. *)
 
-val inspect_decrypt : Guestfs.guestfs -> unit
+val inspect_decrypt : Guestfs.guestfs -> key_store -> unit
 (** Simple implementation of decryption: look for any [crypto_LUKS]
     partitions and decrypt them, then rescan for VGs.  This only works
     for Fedora whole-disk encryption. *)

options/decrypt.c
@@ -68,7 +68,7 @@ make_mapname (const char *device, char *mapname, size_t len)
  * encryption schemes.
  */
 void
-inspect_do_decrypt (guestfs_h *g)
+inspect_do_decrypt (guestfs_h *g, struct key_store *ks)
 {
   CLEANUP_FREE_STRING_LIST char **partitions = guestfs_list_partitions (g);
   if (partitions == NULL)
@@ -82,7 +82,7 @@ inspect_do_decrypt (guestfs_h *g)
       char mapname[32];
       make_mapname (partitions[i], mapname, sizeof mapname);
 
-      CLEANUP_FREE char *key = read_key (partitions[i]);
+      CLEANUP_FREE char *key = get_key (ks, partitions[i]);
       /* XXX Should we call guestfs_luks_open_ro if readonly flag
        * is set?  This might break 'mount_ro'.
        */

options/inspect.c
@@ -65,12 +65,12 @@ compare_keys (const void *p1, const void *p2)
  * This function implements the I<-i> option.
  */
 void
-inspect_mount_handle (guestfs_h *g)
+inspect_mount_handle (guestfs_h *g, struct key_store *ks)
 {
   if (live)
     error (EXIT_FAILURE, 0, _("don’t use --live and -i options together"));
 
-  inspect_do_decrypt (g);
+  inspect_do_decrypt (g, ks);
 
   char **roots = guestfs_inspect_os (g);
   if (roots == NULL)

options/options.h
@@ -102,23 +102,54 @@ struct mp {
   char *fstype;
 };
 
+/* A key in the key store. */
+struct key_store_key {
+  /* The device this key refers to. */
+  char *device;
+
+  enum {
+    key_string,             /* key specified as string */
+    key_file,               /* key stored in a file */
+  } type;
+  union {
+    struct {
+      char *s;              /* string of the key */
+    } string;
+    struct {
+      char *name;           /* filename with the key */
+    } file;
+  };
+};
+
+/* Container for keys, usually collected via the '--key' command line option
+ * in tools.
+ */
+struct key_store {
+  struct key_store_key *keys;
+  size_t nr_keys;
+};
+
 /* in config.c */
 extern void parse_config (void);
 
 /* in decrypt.c */
-extern void inspect_do_decrypt (guestfs_h *g);
+extern void inspect_do_decrypt (guestfs_h *g, struct key_store *ks);
 
 /* in domain.c */
 extern int add_libvirt_drives (guestfs_h *g, const char *guest);
 
 /* in inspect.c */
-extern void inspect_mount_handle (guestfs_h *g);
+extern void inspect_mount_handle (guestfs_h *g, struct key_store *ks);
 extern void inspect_mount_root (guestfs_h *g, const char *root);
-#define inspect_mount() inspect_mount_handle (g)
+#define inspect_mount() inspect_mount_handle (g, ks)
 extern void print_inspect_prompt (void);
 
 /* in key.c */
 extern char *read_key (const char *param);
+extern char *get_key (struct key_store *ks, const char *device);
+extern struct key_store *key_store_add_from_selector (struct key_store *ks, const char *selector);
+extern struct key_store *key_store_import_key (struct key_store *ks, const struct key_store_key *key);
+extern void free_key_store (struct key_store *ks);
 
 /* in options.c */
 extern void option_a (const char *arg, const char *format, struct drv **drvsp);
@@ -218,6 +249,9 @@ extern void free_mps (struct mp *mp);
 
 #define OPTION_x                                \
   guestfs_set_trace (g, 1)
+
+#define OPTION_key                                                      \
+  ks = key_store_add_from_selector (ks, optarg)
 
 #define CHECK_OPTION_format_consumed                                    \
   do {                                                                  \

commit 2c8e81f10e4588d97c1e3a33a989fa28cfc4f3d2

options: Also free ks->keys
valgrind in one of the virt-inspector tests complained that ks->keys
was not being freed.

Updates: commit a34d61ba56a9df9ce294e598cd130decb36a9f48
Fixes: commit f84d95474ccf873a09eb0df9b54554a2d84a886b

options/keys.c
@@ -324,5 +324,6 @@ free_key_store (struct key_store *ks)
     free (key->id);
   }
 
+  free (ks->keys);
   free (ks);
 }