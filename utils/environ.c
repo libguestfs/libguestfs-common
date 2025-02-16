3cb34f6b6898d5fe48afbc62e35fdd81802613d6
Richard W.M. Jones <rjones@redhat.com>
2025-02-16 16:52:33 +0000

    Update copyright dates for 2025
    Automated using this command:
    
    perl -pi.bak -e 's/(20[012][0-9])-20[12][01234]/$1-2025/g' `git ls-files`

--- utils/environ.c
+++ utils/environ.c
@@ -1,5 +1,5 @@
 /* nbdkit
- * Copyright (C) 2018-2023 Red Hat Inc.
+ * Copyright (C) 2018-2025 Red Hat Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
