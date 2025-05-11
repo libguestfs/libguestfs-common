libguestfs/libguestfs-common

commit c92f3cc8249124d51acdf7317eb770d4ce240b6b

mlstdutils: Rename List.dropwhile -> drop_while, takewhile -> take_while
OCaml 5.3 added these functions at long last, but with slightly
different names.  Rename our functions to match.

List.dropwhile is used in libguestfs and virt-v2v.

List.takewhile is not used.

mlstdutils/std_utils.mli
@@ -194,11 +194,11 @@ module List : sig
     val sort_uniq : ('a -> 'a -> int) -> 'a list -> 'a list
     val merge : ('a -> 'a -> int) -> 'a list -> 'a list -> 'a list
 
-    val dropwhile : ('a -> bool) -> 'a list -> 'a list
-    (** [dropwhile f xs] drops leading elements from [xs] until
+    val drop_while : ('a -> bool) -> 'a list -> 'a list
+    (** [drop_while f xs] drops leading elements from [xs] until
         [f] returns false. *)
-    val takewhile : ('a -> bool) -> 'a list -> 'a list
-    (** [takewhile f xs] takes leading elements from [xs] until
+    val take_while : ('a -> bool) -> 'a list -> 'a list
+    (** [take_while f xs] takes leading elements from [xs] until
         [f] returns false.
 
         For any list [xs] and function [f],

mlstdutils/std_utils.ml
@@ -308,14 +308,14 @@ module List = struct
     include List
 
     (* Drop elements from a list while a predicate is true. *)
-    let rec dropwhile f = function
+    let rec drop_while f = function
       | [] -> []
-      | x :: xs when f x -> dropwhile f xs
+      | x :: xs when f x -> drop_while f xs
       | xs -> xs
 
     (* Take elements from a list while a predicate is true. *)
-    let rec takewhile f = function
-      | x :: xs when f x -> x :: takewhile f xs
+    let rec take_while f = function
+      | x :: xs when f x -> x :: take_while f xs
       | _ -> []
 
     let take n xs =

commit 398c266498ce511ba693f1c7abfd409c521e72ff

mlstdutils: Fix comment that still referred to the old function names
Fixes: commit c92f3cc8249124d51acdf7317eb770d4ce240b6b

mlstdutils/std_utils.mli
@@ -71,7 +71,7 @@ module List : sig
         [f] returns false.
 
         For any list [xs] and function [f],
-        [xs = takewhile f xs @ dropwhile f xs] *)
+        [xs = take_while f xs @ drop_while f xs] *)
 
     val take : int -> 'a list -> 'a list
     (** [take n xs] returns the first [n] elements of [xs].  If [xs] is