libguestfs/libguestfs-common

commit d03dd85472cd68644196af5d74093684a4ca435c

mlcustomize/firstboot.ml: Print %USERNAME% and %USERDOMAIN%
It's useful to know which effective user account is running the
firstboot scripts (in case of permissions problems etc.)

Link: https://stackoverflow.com/a/1607292
Thanks: Ming Xie

mlcustomize/firstboot.ml
@@ -295,6 +295,7 @@ exit /b
 
 :main
 echo starting firstboot service
+echo effective user: %USERNAME% domain: %USERDOMAIN%
 
 if not exist "%%scripts_done%%" (
   mkdir "%%scripts_done%%"

commit cf663a835f53f17cb6c937a68af56a32fbc755e2

mlcustomize/firstboot.ml: Fix %-encoding in previous commit
Fixes: commit d03dd85472cd68644196af5d74093684a4ca435c

mlcustomize/firstboot.ml
@@ -295,7 +295,7 @@ exit /b
 
 :main
 echo starting firstboot service
-echo effective user: %USERNAME% domain: %USERDOMAIN%
+echo effective user: %%USERNAME%% domain: %%USERDOMAIN%%
 
 if not exist "%%scripts_done%%" (
   mkdir "%%scripts_done%%"